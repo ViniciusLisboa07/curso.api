ddea1dbf0c51acdee934d61f2820c079cc2126a8
Vinícius Lisboa <viniciuslisboa1001@gmail.com>
2021-06-14 10:48:48 -0300

    Adicionar arquivos de projeto.

---
+++ curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace curso.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logar()
+        {
+            return Ok("", )
+        }
+    }
+}
