1a9e3c56c5e83fdd618d53ccfe7e6eb6f1c492c5
Vinícius Lisboa <viniciuslisboa1001@gmail.com>
2021-06-14 15:18:50 -0300

    login validation

--- curso.api/Models/Usuarios/LoginViewModelInput.cs
+++ curso.api/Models/Usuarios/LoginViewModelInput.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace curso.api.Models.Usuarios
 {
     public class LoginViewModelInput
     {
+        [Required(ErrorMessage = "O login é necessário!")]
         public string Login { get; set; }
 
+
+        [Required(ErrorMessage = "A senha é necessária!")]
         public string Senha { get; set; }
     }
 }
