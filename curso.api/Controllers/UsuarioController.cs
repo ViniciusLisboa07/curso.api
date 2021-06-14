df9b7b6d3093868b7ff405d9a4035bf4cb8be959
Vinícius Lisboa <viniciuslisboa1001@gmail.com>
2021-06-14 16:51:14 -0300

    commit

--- curso.api/Controllers/UsuarioController.cs
+++ curso.api/Controllers/UsuarioController.cs
@@ -8,6 +8,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using curso.api.Filters;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace curso.api.Controllers
 {
@@ -24,12 +28,44 @@ namespace curso.api.Controllers
         [ValidaModelStateCustomizado]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
         {
-/*            if(!ModelState.IsValid)
-            {
-                return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
-            }*/
+            /*            if(!ModelState.IsValid)
+                        {
+                            return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
+                        }*/
 
-            return Ok(loginViewModelInput);
+            var usuarioViewModelOutput = new UsuarioViewModelOutput()
+            {
+                Codigo = 1,
+                Login = "testeqq",
+                Email = "senha@email.com"
+            };
+
+            var secret = Encoding.ASCII.GetBytes("weqwkjhekjhiuyiuyJHAKSdhkajhsdKH12");
+            var symmetricSecurityKey = new SymmetricSecurityKey(secret);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+
+                    new Claim(ClaimTypes.NamesIdentifier, usuarioViewModelOutput.Codigo.ToString()),
+                    new Claim(ClaimTypes.Name, usuarioViewModelOutput.Name.ToString()),
+                    new Claim(ClaimTypes.Email, usuarioViewModelOutput.Email.ToString())
+
+                }),
+
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+        };
+
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokenGenereted = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(tokenGenereted);
+
+
+            return Ok(new 
+            {
+                Token = token,
+               Usuario = usuarioViewModelOutput
+            });
         }
 
         [HttpPost]
