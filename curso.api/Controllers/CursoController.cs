371fe9ac2853d8fa9e0ad1f9c05810ed84981146
Vinícius Lisboa <viniciuslisboa1001@gmail.com>
2021-06-15 09:02:14 -0300

    commit

---
+++ curso.api/Controllers/CursoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using curso.api.Models.Cursos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace curso.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao cadastrar"), Type = typeof(CursoViewModelInput))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Post(CursoViewModelInput cursoViewModelInput)
+        {
+            var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            return Created("", cursoViewModelInput);
+        }
+    }
+}
--- curso.api/Startup.cs
+++ curso.api/Startup.cs
@@ -39,36 +39,6 @@ namespace curso.api
 
             services.AddSwaggerGen(c =>
             {
-                services.AddSwaggerGen(c =>
-                {
-                    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                    {
-                        Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
-                        Name = "Authorization",
-                        In = ParameterLocation.Header,
-                        Type = SecuritySchemeType.ApiKey,
-                        Scheme = "Bearer"
-                    });
-                    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
-                });
-
-                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                    c.IncludeXmlComments(xmlPath);
-                });
-
                 var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
