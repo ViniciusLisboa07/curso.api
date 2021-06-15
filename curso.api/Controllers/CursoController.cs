97b976e0526dcae045bac3ec270be60e2f8ee250
Vinícius Lisboa <viniciuslisboa1001@gmail.com>
2021-06-15 09:19:40 -0300

    commit

--- curso.api/Controllers/CursoController.cs
+++ curso.api/Controllers/CursoController.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using curso.api.Models.Cursos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace curso.api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/cursos")]
     [ApiController]
+    [Authorize]
     public class CursoController : ControllerBase
     {
-        [SwaggerResponse(statusCode: 201, description: "Sucesso ao cadastrar"), Type = typeof(CursoViewModelInput))]
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao cadastrar", Type = typeof(CursoViewModelInput))]
         [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
         [HttpPost]
         [Route("")]
@@ -22,6 +24,28 @@ namespace curso.api.Controllers
         {
             var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
             return Created("", cursoViewModelInput);
+        }
+
+        /// <summary>
+        /// Este serviço permite obter todos os cursos ativos do usuário.
+        /// </summary>
+        /// <returns>Retorna status ok e dados do curso do usuário</returns>
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao obter os cursos", Type = typeof(CursoViewModelOutput))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get()
+        {
+            var cursos = new List<  CursoViewModelOutput >();
+            //var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            cursos.Add(new CursoViewModelOutput
+            {
+                Login = "",
+                Descricao = "teste",
+                Nome = "teteste"
+            });
+
+            return Ok(cursos);
         }
     }
 }
--- curso.api/Startup.cs
+++ curso.api/Startup.cs
@@ -77,6 +77,7 @@ namespace curso.api
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
